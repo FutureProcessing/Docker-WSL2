4e445217cdd5a00766d18cca92f4efbaedcb6dd7
Paweł Szczekutowicz <pszczekutowicz@future-processing.com>
2021-10-07 18:54:40 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,11 @@ if [[ ! -f $DOCKER_LIST_PATH ]]; then
   echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee $DOCKER_LIST_PATH > /dev/null
   sudo apt update --yes
 fi
+
 sudo apt install --yes docker-ce
+
 sudo usermod -aG docker $(whoami)
+
 sudo service docker start
+
+docker info  --format "Docker Server version: {{.ServerVersion}}"
